2e9fcbe363bacf6ccb892941b12eb6a9ae54f91f
n1k0lay <kolia.d97@gmail.com>
2023-05-14 23:33:19 +0500

    feat: Подсветка открытой страницы

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,17 +9,18 @@ import icon6 from '../../assets/icons/H3-5.svg'
 import icon7 from '../../assets/icons/H3-6.svg'
 import styles from './NavBar.module.scss'
 import cn from 'classnames';
-import {Link} from "react-router-dom";
-
+import {Link, useLocation, useNavigate} from "react-router-dom";
 
 const NavBar = ({className}) => {
+
+    const {pathname} = useLocation();
     return (
         <div className={cn(className, styles.navbar)}>
             <img className={styles.logo} src={logoH2O} alt="logo h2o" width={64}/>
             <nav className={styles.menu}>
                 <ul>
-                    <li><Link to="/home"><img src={icon7} alt="icon7" width={33}/></Link></li>
-                    <li><Link to="/table"><img src={icon1} alt="icon1" width={33}/></Link></li>
+                    <li className={cn({[styles.active]: pathname === '/home'})}><Link to="/home"><img src={icon7} alt="icon7" width={33}/></Link></li>
+                    <li className={cn({[styles.active]: pathname === '/table'})}><Link to="/table"><img src={icon1} alt="icon1" width={33}/></Link></li>
                     <li><Link to="/"><img src={icon2} alt="icon2" width={33}/></Link></li>
                     <li><Link to="/"><img src={icon3} alt="icon3" width={33}/></Link></li>
                     <li><Link to="/"><img src={icon4} alt="icon4" width={33}/></Link></li>
